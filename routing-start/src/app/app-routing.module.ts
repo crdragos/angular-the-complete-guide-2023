b452e2a0a790372c2fc2fd9fb53453b94fe57e98
crdragos <dcrecan@icloud.com>
2023-05-29 14:50:31 +0300

    section 11: routing basics

---
+++ routing-start/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {UsersComponent} from "./users/users.component";
+import {UserComponent} from "./users/user/user.component";
+import {ServersComponent} from "./servers/servers.component";
+import {ServerComponent} from "./servers/server/server.component";
+import {EditServerComponent} from "./servers/edit-server/edit-server.component";
+import {AuthGuard} from "./auth-guard.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolver} from "./servers/server/server-resolver.service";
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id', component: UserComponent},
+    ],
+  },
+  {
+    path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent, resolve: {server: ServerResolver}},
+      {path: ':id/edit', canDeactivate: [CanDeactivateGuard], component: EditServerComponent},
+    ],
+  },
+  {path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'}},
+  // {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'},
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+    // RouterModule.forRoot(appRoutes, {useHash: true})
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+
+}
--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,27 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { ServersService } from '../servers.service';
+import {ServersService} from '../servers.service';
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {CanComponentDeactivate} from "./can-deactivate-guard.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
+  server: { id: number, name: string, status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit: boolean;
+  saveChanges: boolean = false;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.allowEdit = params['allowEdit'] === '1';
+      }
+    );
+    this.route.fragment.subscribe();
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.saveChanges = true;
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.saveChanges) {
+      return confirm('Do you want to discard your changes?');
+    } else {
+      return true;
+    }
   }
 
 }
