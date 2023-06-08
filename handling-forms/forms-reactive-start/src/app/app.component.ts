7a97d22d9bb984894b55f4cfe24cc496d89c900f
crdragos <dcrecan@icloud.com>
2023-06-08 10:36:12 +0300

    section 15 - reactive: create form + sync form + submit form

--- handling-forms/forms-reactive-start/src/app/app.component.ts
+++ handling-forms/forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders: string[] = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male'),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.signUpForm);
+  }
 }
