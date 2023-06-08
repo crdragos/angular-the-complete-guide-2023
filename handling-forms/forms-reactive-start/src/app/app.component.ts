3cb00e2c9d7d953dca911070f238118d26302e4d
crdragos <dcrecan@icloud.com>
2023-06-08 11:24:40 +0300

    section 15 - reactive: add async validators

--- handling-forms/forms-reactive-start/src/app/app.component.ts
+++ handling-forms/forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.isValidUsername.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.isForbiddenEmail),
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([]),
@@ -40,5 +41,17 @@ export class AppComponent implements OnInit {
       return {forbidden: true};
     }
     return null;
+  }
+
+  isForbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
   }
 }
